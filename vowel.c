9ef07005ecd5a384977d247ea6956ce6d8d3193e
manisudarshan <35294171+manisudarshan@users.noreply.github.com>
2018-01-17 16:26:09 +0530

    vowel
    program

---
+++ vowel.c
@@ -0,0 +1,12 @@
+#include<studio.h>
+void main()
+{
+char ch;
+printf{"\nEnter the character"};
+scanf{"%d",&ch};
+if(ch=='a'//ch=='A'//ch=='e'//ch=='E'//ch=='i'//ch=='I'//ch=='o'//ch=='O'//ch=='u'//ch=='U')
+printf{"\n The given character is vowel"};
+else
+printf{"\n The given character is consonant"};
+getch();
+}
