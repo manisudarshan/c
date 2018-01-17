fe0c0535e65d1d94160486bc15f859515fcadb0b
manisudarshan <35294171+manisudarshan@users.noreply.github.com>
2018-01-17 16:44:58 +0530

    alphabet
    program

---
+++ alphabet.c
@@ -0,0 +1,12 @@
+#include<studio.h>
+void main()
+{
+cha ch;
+printf{"\n Enter the character"};
+scanf{"%c"&ch};
+if((ch>='a'&&ch<='z')//(ch>='A'&&ch<='Z'))
+printf{"\n The character %c is an alphabet",ch};
+else
+printf{"\n The character %c is not an alphabet",ch};
+getch();
+}
