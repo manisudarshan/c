b959bff830db3e3e255035cb9e401376bb25963e
manisudarshan <35294171+manisudarshan@users.noreply.github.com>
2018-01-17 16:07:13 +0530

    program for odd or even
    program file

---
+++ odd.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf{"\n Enter the number"};
+scanf{"%d",&n};
+if(n%2==0)
+printf{"\n The given number is even"};
+else
+printf{"\n The given number is odd"};
+getch();
+}
